21395fcef7d8f9dd3302670a48d8ec6382003af8
Emanuell95 <emanuelmendozanieves@Emanuels-MacBook-Pro.local>
2025-01-19 18:24:20 -0400

    file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-# Import the add function from add_0.py
 if __name__ == "__main__":
     from add_0 import add
 
-    # Assign values to a and b
     a = 1
     b = 2
-
-    # Print the result using string formatting
     print("{} + {} = {}".format(a, b, add(a, b)))
-
