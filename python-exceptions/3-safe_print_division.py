4db54cc04c9e7b8f5979e5dd1ed5b40c35a05b9a
Emanuell95 <emanuelmendozanieves@Emanuels-MacBook-Pro.local>
2025-01-25 18:39:28 -0400

    Dividing two integers

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
