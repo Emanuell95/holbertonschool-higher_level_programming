f6434c7d1198885bcff7435d562ab70394ac3ede
Emanuell95 <emanueltute95@gmail.com>
2025-02-03 17:38:20 -0400

    Improving geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+class BaseGeometry:
+    def area(self):
+        """
+        Calculates the area of a given shape.
+
+        Raises:
+            Exception: area() is not implemented for this shape
+        """
+        raise Exception("area() is not implemented")
