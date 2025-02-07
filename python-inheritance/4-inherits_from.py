a46819c35c6ca682f8f3f847077b9c911c4144a1
Emanuell95 <emanueltute95@gmail.com>
2025-02-07 12:47:23 -0400

    4-inheritances

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
 """ 
-This module provides a function to check if an object is an instance of a specified class 
-or if it is an instance of a class that inherited from the specified class.
+This module provides functions to check the inheritance and instance relationships of objects.
 
 Functions:
-    inherits_from(obj, a_class): 
+    is_kind_of_class(obj, a_class): 
         Determines whether an object is an instance of a class or an instance of a subclass of that class.
+    inherits_from(obj, a_class):
+        Determines whether an object is an instance of a class that inherited (directly or indirectly) from a specified class.
 """
-def inherits_from(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
-    Checks if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class.
+    Checks if the object is an instance of, or if the object is an instance of a class that
+    inherited from, the specified class.
 
     Args:
         obj (object): The object to check.
         a_class (class): The class to check against.
 
     Returns:
-        bool: True if obj is an instance of a_class, False otherwise.
+        bool: True if obj is an instance of a_class or a subclass, False otherwise.
     """
-    return type(obj) == a_class
+    return isinstance(obj, a_class)
+
+def inherits_from(obj, a_class):
+    """
+    Checks if the object is an instance of a class that inherited (directly or indirectly)
+    from the specified class.
+
+    Args:
+        obj (object): The object to check.
+        a_class (class): The class to check against.
+
+    Returns:
+        bool: True if obj is an instance of a subclass of a_class, False otherwise.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
