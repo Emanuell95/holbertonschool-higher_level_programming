41d3b1eee6586156a6a7a45e9b874fa839c79113
Emanuell95 <emanueltute95@gmail.com>
2025-02-07 13:16:07 -0400

    6-basegeometry updating

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
-class BaseGeometry:
-    """
-    A class representing a base geometry.
-    The class provides a method to calculate the area of a shape.
-    If the area is not implemented for a specific shape, an exception is raised.
-    """
-    def area(self):
-        """
-        Calculates the area of a given shape.
+"""
+This module provides functions to check the inheritance and instance relationships of objects.
 
-        Raises:
-            Exception: area() is not implemented for this shape
-        """
-        raise Exception("area() is not implemented")
+Functions:
+    is_kind_of_class(obj, a_class):
+        Determines whether an object is an instance of a class or an instance of a subclass of that class.
+    inherits_from(obj, a_class):
+        Determines whether an object is an instance of a subclass (directly or indirectly) of a specified class,
+        but not an instance of the specified class itself.
+"""
+def is_kind_of_class(obj, a_class):
+    """
+    Determines if an object is an instance of a class or an instance of a subclass of that class.
+
+    Args:
+        obj (object): The object to check.
+        a_class (type): The class to check against.
+
+    Returns:
+        bool: True if obj is an instance of a_class or a subclass, False otherwise.
+    """
+    return isinstance(obj, a_class)
+
+def inherits_from(obj, a_class):
+    """
+    Determines if an object is an instance of a subclass (directly or indirectly) of a specified class,
+    but not an instance of the specified class itself.
+
+    Args:
+        obj (object): The object to check.
+        a_class (type): The class to check against.
+
+    Returns:
+        bool: True if obj is an instance of a subclass of a_class, but not a direct instance of a_class.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
