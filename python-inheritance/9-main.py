69b8f4512ce3005e21927e381011c1db5dc8e1a5
Emanuell95 <emanueltute95@gmail.com>
2025-02-07 13:26:05 -0400

    9-rectangle update

--- python-inheritance/9-main.py
+++ python-inheritance/9-main.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module demonstrates the usage of the Rectangle class.
+It creates a Rectangle object and prints its area
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 r = Rectangle(3, 5)
