a5c24dbb77232ef4ebf7e5a5352ba8b862d2d870
Emanuell95 <emanueltute95@gmail.com>
2025-02-07 12:36:26 -0400

    Adding interpreter python to file

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def inherits_from(obj, a_class):
     """
     Checks if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class.
