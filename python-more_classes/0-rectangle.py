9b29ebc683887e3d1ec860eb20765117cdc00066
Emanuell95 <emanueltute95@gmail.com>
2025-02-10 16:51:04 -0400

    update1 of  0-rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -4,6 +4,6 @@ class Rectangle:
     A class used to represent a Rectangle.
 
     This class currently does not include any attributes or methods but serves as a blueprint 
-    for defining a rectangle in future implementations.
+    for defining a rectangle in future implementations. This class is empty.
     """
     pass
