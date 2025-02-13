576d5a72c5ae0c0376d08de7d69e0c6f8aeb1e61
Emanuell95 <emanueltute95@gmail.com>
2025-02-13 17:10:10 -0400

    class to JSON

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-"""Defines a class_to_json function."""
+"""Module containing the function class_to_json"""
+
+
 def class_to_json(obj):
-    """Returns the dictionary description for JSON serialization of an object."""
+    """Returns the dictionary description with simple data structure,
+    (list, dictionary, string, integer and boolean) for JSON serialization,
+    of an object.
+
+    Args:
+        obj (MyClass): object.
+
+    Returns:
+        dict: dictionary.
+    """
+    
     return obj.__dict__
